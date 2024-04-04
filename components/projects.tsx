1eb8cdc6b41d7ef8d5d2302cd1b0c31d5234250d
Xinrong Shi <sxr1207@gmail.com>
2024-04-04 20:44:56 +1300

    Added skills section.

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
   
   return (
-    <section ref={ref} className="scroll-mt-28" id="projects">
+    <section ref={ref} className="scroll-mt-28 mb-28" id="projects">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
